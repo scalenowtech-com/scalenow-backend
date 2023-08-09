c95c3f702685ce25fdf32c32e6019bbb0644d882
Kingshuk Deb <itskingshukdeb@gmail.com>
2023-08-09 21:08:42 +0530

    update req body type

--- src/contact/service.ts
+++ src/contact/service.ts
@@ -1,16 +1,34 @@
 import { Response } from "express";
 import { createContactForm } from "./db";
+import { ContactDTO } from "./schema";
 
 const saveContactLead = async (req: any, res: Response) => {
   try {
-    const data = req.body;
-    const { data: contactLead, error } = await createContactForm(data);
+    const data: {
+      organisationName: string;
+      industries: string[];
+      servicesNeeded: string[];
+      fullName: string;
+      position: string;
+      businessPhoneNumber: string;
+      companyEmail: string;
+    } = req.body;
+
+    const createData: ContactDTO = {
+      ...data,
+      industries: JSON.stringify(data.industries),
+      servicesNeeded: JSON.stringify(data.servicesNeeded),
+    };
+
+    const { data: contactLead, error } = await createContactForm(createData);
+
     if (error !== null) {
       return res.status(400).json({
         data: null,
         error,
       });
     }
+
     return res.json({
       data: contactLead,
       error,
